e881a16705058ded91ec360c942cb1411aff570b
Marat <m.bektemirov@big3.ru>
2022-10-27 07:49:29 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function calculateWaterAmount(landscape) {
-    const heightHash = {};
+    const lastLengthByHeight = {};
 
     let sum = 0;
 
     for(let length=0; length<landscape.length; length++){
         for(let height=0; height<landscape[length]; height++){
-            sum += heightHash.hasOwnProperty(height) 
-                ? (length - heightHash[height]) - 1  
-                : 0;
-
-            heightHash[height] = length;
+            if (lastLengthByHeight[height]) {
+                sum += (length - lastLengthByHeight[height]) - 1;
+            }
+   
+            lastLengthByHeight[height] = length + '';
         }
     }
 
--- test.js
+++ test.js
@@ -1,7 +1,11 @@
-const {calculateWaterAmount} = require('./index');
+const { calculateWaterAmount } = require('./index');
 
 test('data from task', () => {
   expect(calculateWaterAmount([5,2,3,4,5,4,0,3,1])).toBe(9);
+});
+
+test('custom data', () => {
+  expect(calculateWaterAmount([4,2,3,4,5,4,0,3,1])).toBe(6);
 });
 
 test('plateau', () => {
@@ -10,4 +14,16 @@ test('plateau', () => {
 
 test('identical skyscrapers', () => {
   expect(calculateWaterAmount([100,0,0,100,0,0,100])).toBe(400);
+});
+
+test('houses between identical skyscrapers', () => {
+  expect(calculateWaterAmount([100,0,1,100,0,1,100])).toBe(398);
+});
+
+test('large puddle', () => {
+  expect(calculateWaterAmount([2,3,1,3,1,2,1,6,6,6])).toBe(7);
+});
+
+test('two houses', () => {
+  expect(calculateWaterAmount([4,0,3])).toBe(3);
 });
